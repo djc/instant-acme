4668bc35ca876d1146c0364028faa5a116f604f3
dvdsk <noreply@davidsk.dev>
2023-04-23 18:07:41 +0200

    cargo fmt

--- src/lib.rs
+++ src/lib.rs
@@ -246,7 +246,7 @@ impl Account {
         Ok(Order {
             account: self.inner.clone(),
             nonce,
-            // Order of fields matters! We return errors from Problem::check 
+            // Order of fields matters! We return errors from Problem::check
             // before emitting an error if there is no order url. Or the
             // simple no url error hides the causing error in `Problem::check`.
             state: Problem::check::<OrderState>(rsp).await?,
