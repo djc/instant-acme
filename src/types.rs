c51f696133611b84ee94127b60645bc74745eb3e
Dirkjan Ochtman <dirkjan@ochtman.nl>
2024-02-05 13:50:45 +0100

    Proactively add other new fields

--- src/types.rs
+++ src/types.rs
@@ -356,7 +356,9 @@ pub(crate) struct DirectoryUrls {
     // The fields below were added later and old `AccountCredentials` may not have it.
     // Newer deserialized account credentials grab a fresh set of `DirectoryUrls` on
     // deserialization, so they should be fine. Newer fields should be optional, too.
+    pub(crate) new_authz: Option<String>,
     pub(crate) revoke_cert: Option<String>,
+    pub(crate) key_change: Option<String>,
 }
 
 #[derive(Serialize)]
