b89eb768e039f63af4cbcfba06d171b378c83a6d
Daniel McCarney <daniel@binaryparadox.net>
2025-01-27 15:08:14 -0500

    examples: simple test PKI and rcgen generator
    Populate a tests/testdata directory with a PEM encoded CA certificate,
    a PEM encoded server certificate issued by the CA, and the PEM encoded
    private key for the server certificate.
    
    All of the above is generated by the `certgen.rs` example using `rcgen`.
    Run with: `cargo run --example certgen`

---
+++ examples/certgen.rs
@@ -0,0 +1,30 @@
+//! This "example" is used for generating integration test certificates.
+//!
+//! It is not intended to be an example of using `instant-acme`.
+
+use std::fs;
+
+use rcgen::{BasicConstraints, DistinguishedName, DnType, IsCa, KeyPair};
+
+fn main() -> anyhow::Result<()> {
+    let ca_key = KeyPair::generate()?;
+    let mut distinguished_name = DistinguishedName::new();
+    distinguished_name.push(DnType::CommonName, "Pebble CA".to_owned());
+    let mut ca_params = rcgen::CertificateParams::default();
+    ca_params.distinguished_name = distinguished_name;
+    ca_params.is_ca = IsCa::Ca(BasicConstraints::Unconstrained);
+
+    let ca_cert = ca_params.self_signed(&ca_key)?;
+    fs::write("tests/testdata/ca.pem", ca_cert.pem())?;
+
+    let ee_key = KeyPair::generate()?;
+    fs::write("tests/testdata/server.key", ee_key.serialize_pem())?;
+
+    let mut ee_params =
+        rcgen::CertificateParams::new(["localhost".to_owned(), "127.0.0.1".to_owned()])?;
+    ee_params.distinguished_name = DistinguishedName::new();
+    let ee_cert = ee_params.signed_by(&ee_key, &ca_cert, &ca_key)?;
+    fs::write("tests/testdata/server.pem", ee_cert.pem())?;
+
+    Ok(())
+}
