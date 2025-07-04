0e55e6d15f1bc6cef04782bef8f17be7d2804bb0
Dirkjan Ochtman <dirkjan@ochtman.nl>
2025-07-04 13:55:49 +0200

    Add DefaultClient::with_config()

--- src/lib.rs
+++ src/lib.rs
@@ -18,6 +18,10 @@ use http::{Method, Request, Response, StatusCode};
 use http_body_util::{BodyExt, Full};
 use httpdate::HttpDate;
 #[cfg(feature = "hyper-rustls")]
+use hyper_rustls::HttpsConnectorBuilder;
+#[cfg(feature = "hyper-rustls")]
+use hyper_rustls::builderstates::WantsSchemes;
+#[cfg(feature = "hyper-rustls")]
 use hyper_util::client::legacy::Client as HyperClient;
 #[cfg(feature = "hyper-rustls")]
 use hyper_util::client::legacy::connect::{Connect, HttpConnector};
@@ -190,19 +194,25 @@ pub struct DefaultClient(HyperClient<hyper_rustls::HttpsConnector<HttpConnector>
 
 #[cfg(feature = "hyper-rustls")]
 impl DefaultClient {
+    /// Create a new default HTTP client with the given TLS configuration
+    pub fn with_config(config: rustls::ClientConfig) -> Self {
+        Self::new(hyper_rustls::HttpsConnectorBuilder::new().with_tls_config(config))
+    }
+
     /// Create a new default client using rustls-platform-verifier
     pub fn try_new() -> Result<Self, Error> {
-        Ok(Self(
-            HyperClient::builder(TokioExecutor::new()).build(
-                hyper_rustls::HttpsConnectorBuilder::new()
-                    .try_with_platform_verifier()
-                    .map_err(|e| Error::Other(Box::new(e)))?
-                    .https_only()
-                    .enable_http1()
-                    .enable_http2()
-                    .build(),
-            ),
+        Ok(Self::new(
+            hyper_rustls::HttpsConnectorBuilder::new()
+                .try_with_platform_verifier()
+                .map_err(|e| Error::Other(Box::new(e)))?,
         ))
+    }
+
+    fn new(builder: HttpsConnectorBuilder<WantsSchemes>) -> Self {
+        Self(
+            HyperClient::builder(TokioExecutor::new())
+                .build(builder.https_only().enable_http1().enable_http2().build()),
+        )
     }
 }
 
